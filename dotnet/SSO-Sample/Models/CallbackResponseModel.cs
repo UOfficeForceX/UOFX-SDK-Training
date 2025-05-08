9ca12360b562c665c7b127b948843c909d72e222
UOFX_Customize <UOFX_Customize@edetw.com>
2025-05-08 15:49:18 +0000

    Sync with Azure.Devops

---
+++ dotnet/SSO-Sample/Models/CallbackResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SSO_Sample.Models
+{
+    public class CallbackResponseModel
+    {
+        public string AccountKey { get; set; }
+
+        public long Timestamp { get; set; }
+    }
+}
