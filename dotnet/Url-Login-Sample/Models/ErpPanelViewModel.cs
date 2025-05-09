e92cb6feb4f930918f93e4482d27c360be48cd4c
UOFX_Customize <UOFX_Customize@edetw.com>
2025-05-09 09:31:25 +0000

    Sync with Azure.Devops

---
+++ dotnet/Url-Login-Sample/Models/ErpPanelViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Ede.Uofx.PubApi.Sdk.NetStd.Models;
+using Ede.Uofx.PubApi.Sdk.NetStd.Models.Bpm;
+
+namespace Url_Login_Sample.Models
+{
+    public class ErpPanelViewModel
+    {
+        public AllCanApplyFormViewModel allCanApplyForm { get; set; }
+        public SearchByPage<SearchFormByApplyResultModel> applyForm { get; set; }
+        public SearchByPage<SearchFormByAwaitingResultModel> awaitingForm { get; set; }
+    }
+}
